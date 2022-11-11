d164edf84675dd4b044ab03e335da5a829f7d376
Camila Eduarda Servat <cs004259@cfjl.com.br>
2022-11-11 19:43:02 -0300

    Criação da classe DAO e métodos mágicos

--- public/index.php
+++ public/index.php
@@ -1,7 +1,3 @@
 <?php
-    
-require_once __DIR__ . '/../vendor/autoload.php';
 
-echo date('d/m/Y H:i:s');
-echo '<br>';
-echo FRONTEND_TITLE;
+require_once __DIR__ . '/../vendor/autoload.php';
---
+++ src/Core/Attribute/Entidade.php
@@ -0,0 +1,8 @@
+<?php
+namespace Petshop\Core\Attribute;
+
+#[\Attribute]
+class Entidade 
+{
+
+}
