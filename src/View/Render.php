6c4f061519fc4d2387c74124e533b65dd55813ad
Camila Eduarda Servat <cs004259@cfjl.com.br>
2022-11-17 21:23:22 -0300

    Criada Render e método q renderiza frontend pages

--- src/View/Render.php
+++ src/View/Render.php
@@ -2,7 +2,39 @@
 
 namespace Petshop\View;
 
+use Exception;
+
 class Render 
 {
-    
+    /**
+     * Método que carrega uma página com estrutura do Frontend,
+     * recebe dois parâmetros
+     *
+     * @param string $pagina Nome do arquivo a ser impresso
+     * @param array $dados Dados a serem inseridos na página
+     * @return void
+     */
+    static public function front(string $pagina, array $dados=[])
+    {
+        //monta o caminho do local onde a página está 
+        $pathPagina = TFRONTEND . 'pages/' . $pagina . '.php';
+
+        if (!file_exists($pathPagina)) {
+            error_log('Página template não localizada em: '.$pathPagina);
+            throw new Exception("A página solicitada '{$pagina}' não foi localizada");
+        }
+
+        if (empty($dados['titulo'])) {
+            $dados['titulo'] = FRONTEND_TITLE;
+        } else {
+            $dados['titulo'] = $dados['titulo'] . ' - ' . FRONTEND_TITLE;
+        }
+
+        //transforma os índeices do vetor em variáveis
+        extract($dados);
+
+        require_once TFRONTEND . 'common/top.php';
+        require_once $pathPagina;
+        require_once TFRONTEND . 'common/bottom.php';
+    }
 }
