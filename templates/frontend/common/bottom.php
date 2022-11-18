afda7b03529fbd9b2da2617c9b467af08979a2c9
Camila Eduarda Servat <cs004259@cfjl.com.br>
2022-11-17 21:09:53 -0300

    Criados arquivos base de layout

---
+++ templates/frontend/common/bottom.php
@@ -0,0 +1,10 @@
+    
+    <!-- BOOTSTRAP JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!-- FONT-AWESOME -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js" integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- SCRIPTS DO PROJETO -->
+    <script src="/assets/js/script.js"></script>
+</body>
+</html>
