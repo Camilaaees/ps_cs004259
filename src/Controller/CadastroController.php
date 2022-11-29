616efb84c1cdec3ff971a1f4cec3d2540d92e46e
Camila Eduarda Servat <cs004259@cfjl.com.br>
2022-11-29 20:21:49 -0300

    Criada estrutura para cadastro de cliente

--- src/Controller/CadastroController.php
+++ src/Controller/CadastroController.php
@@ -20,6 +20,25 @@ class CadastroController extends FrontController
 
     private function formCadastro()
     {
-        
+        $dados = [
+            'btn_label'=>'Criar minha Conta',
+            'btn_class'=>'btn btn-success mt-5',
+            'fields'=> [
+                ['type'=>'radio-inline', 'class'=>'col-6', 'label'=>'Você é pessoa...', 'name'=>'tipo', 
+                    'options'=>[
+                        ['label'=>'Física', 'value'=>'F'],
+                        ['label'=>'Jurídica', 'value'=>'J']
+                    ],
+                    'required'=>true, 
+                ],
+                ['type'=>'text', 'class'=>'col-6', 'label'=>'Documento', 'name'=>'cpfcnpj', 'required'=>true],
+                ['type'=>'text', 'name'=>'nome', 'label'=>'Seu Nome Completo', 'required'=>true],
+                ['type'=>'email', 'name'=>'email', 'label'=>'Seu melhor E-mail', 'required'=>true],
+                ['type'=>'password', 'class'=>'col-6', 'name'=>'senha', 'label'=>'Crie uma Senha', 'required'=>true],
+                ['type'=>'password', 'class'=>'col-6', 'name'=>'senha2', 'label'=>'Confirme a Senha', 'required'=>true],
+            ]
+        ];
+
+        return Render::block('form', $dados);
     }
 }
