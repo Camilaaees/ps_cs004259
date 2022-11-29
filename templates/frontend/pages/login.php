907dcac4f0317b732d1fab4e797f4f547257f8df
Camila Eduarda Servat <cs004259@cfjl.com.br>
2022-11-28 21:50:43 -0300

    Iniciando o Login

--- templates/frontend/pages/login.php
+++ templates/frontend/pages/login.php
@@ -1,1 +1,12 @@
-Olá, estamos na página de login
+<div class="containr my-5">
+    <div class="row">
+        <div class="col-3 ms-auto border-end">
+            <h3 class="text-center display-6 pb-3 border-bottom mb-3">Fazer Logon</h3>
+            <?=$formLogin?>
+        </div>
+        <div class="col-3 me-auto">
+            Processo de Cadastro
+        </div>
+    </div>
+
+</div>
