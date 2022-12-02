bd1db4b0b9426efa5d21e06ae55acc3d06e2162e
Camila Eduarda Servat <cs004259@cfjl.com.br>
2022-12-02 19:26:05 -0300

    Criado início da página de meus-dados

--- templates/frontend/pages/meus-dados.php
+++ templates/frontend/pages/meus-dados.php
@@ -1,1 +1,21 @@
-exibindo meus dados de cliente 
+<?php
+    $cliente = $_SESSION['cliente'];
+    $cliente['prinome'] = substr($cliente['nome'], 0, strpos($cliente['nome'], ' '));
+?>
+<div class="container my-5">
+    <div class="row">
+        <div class="col-3 text-center bg-secondary text-light py-2 rounded-2">
+            <div>Bem Vindo(a): <strong><?=$cliente['prinome']?></strong> <br></div>
+            <div style="font-size: .8em;">(<?=$cliente['email']?>)<br></div>
+            <div class="mt-5">
+                <a href="/logout" class="badge text-bg-danger text-decoration-none">Sair</a>
+            </div>
+
+        </div>
+        <div class="col-9 ps-5">
+            - Meus pedidos <br>
+            - Meus Endereços <br>
+            - Meus Favoritos
+        </div>
+    </div>
+</div>
