7a9cfd74bba6a7da2af028e25820a26e2718f52d
Camila Eduarda Servat <cs004259@cfjl.com.br>
2022-12-02 20:19:42 -0300

    Usando informações d sessão d cliente nas páginas

--- templates/frontend/blocks/topo.php
+++ templates/frontend/blocks/topo.php
@@ -1,3 +1,28 @@
+<?php
+if (empty($cliente)) {
+    $opcaoLogin = <<<HTML
+        <a href="/login" title="Entrar/Cadastrar" class="d-flex align-items-center lh-1">
+            <i class="bi bi-person fs-3 pe-2"></i>
+            <span>Entrar ou <br>Cadastrar</span>
+        </a>
+        HTML;
+} else {
+    $opcaoLogin = <<<HTML
+    <div class="dropdown">
+        <i class="bi bi-person fs-3 pe-1"></i>
+        <a href="#" class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            Olá <strong>{$cliente['prinome']}</strong>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark border-light">
+            <li><a class="dropdown-item" href="/meus-dados">Minha Área</a></li>
+            <li><a class="dropdown-item" href="/meus-pedidos">Meus Pedidos</a></li>
+            <li><hr class="dropdown-divider border-light"></li>
+            <li><a class="dropdown-item" href="/logout">Sair</a></li>
+        </ul>
+    </div>
+    HTML;
+}
+?>
 <!--Hack para o topo fixo não "comer" o topo da página-->
 <div style="margin-top: 5.5em">&nbsp;</div>
 
@@ -15,12 +40,9 @@
                     <button type="submit" class="btn-busca"><i class="bi bi-search"></i></button>
                 </form>
             </div>
-            <div class="topo-site-opcoes col-4 row text-center align-items-center">
-                <div class="topo-site-usr col-8">
-                    <a href="/login" title="Entrar/Cadastrar" class="d-flex align-items-center">
-                        <i class="bi bi-person fs-3 pe-2"></i>
-                        <span>Entre ou Cadastre-se</span>
-                    </a>
+            <div class="topo-site-opcoes col-4 row align-items-center">
+                <div class="topo-site-opcoes-usr col-8">
+                    <?= $opcaoLogin ?>
                 </div>
                 <div class="col-4 d-flex justify-content-between">
                     <a href="/favoritos" title="Ver meus Favoritos" class="px-2">
@@ -55,7 +77,7 @@
             </div>
             <div class="topo-site-inferior-fone col-4 d-flex align-items-center justify-content-end">
                 <i class="bi bi-telephone pe-2"></i>
-                <span><?=$telefone1??'' ?></span>
+                <span><?= $telefone1 ?? '' ?></span>
             </div>
         </div>
     </div>
